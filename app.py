d215c03377675cecabff890078882758bd68a5fc
Murat <borkybelg@gmail.com>
2026-03-14 19:36:57 +0100

    Update app.py

--- app.py
+++ app.py
@@ -74,7 +74,8 @@ if check_password():
     m_tickers = {
         "DAX": "^GDAXI", "S&P 500": "^GSPC", "Nasdaq": "^NDX", "Dow Jones": "^DJI",
         "Gold": "GC=F", "Silber": "SI=F", "Öl": "BZ=F", "VIX": "^VIX",
-        "BTC-USD": "BTC-USD", "ETH-USD": "ETH-USD", "ETH-EUR": "ETH-EUR", "EUR/TRY": "EURTRY=X"
+        "BTC-USD": "BTC-USD", "ETH-USD": "ETH-USD", "ETH-EUR": "ETH-EUR", 
+        "SDAX": "^SDAXI", "TecDAX": "^TECDAX", "Russell 2k": "^RUT", "EUR/TRY": "EURTRY=X"
     }
     m_cols = st.columns(6)
     for i, (n, s) in enumerate(m_tickers.items()):
