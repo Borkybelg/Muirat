419cd12f7a2616966df6b33cd7a24d40655d1914
Murat <borkybelg@gmail.com>
2026-03-31 10:06:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -251,7 +251,7 @@ m_tickers = {
     "DAX": "^GDAXI", "S&P 500": "^GSPC", "Nasdaq": "^NDX", "Dow Jones": "^DJI",
     "SDAX": "^SDAXI",  "MDAX": "^MDAXI", "TecDAX": "^TECDAX", "Russell 2k": "^RUT", 
     "Nikkei 225": "^N225", "China 50": "XIN9.FGI", "Kospi": "KOSPI-3.KS","BTC-USD": "BTC-USD", "ETH-USD": "ETH-USD", "ETH-EUR": "ETH-EUR", 
-    "Gold": "GC=F", "Silber": "SI=F", "Öl": "BZ=F", "VIX": "^VIX", "EUR/USD": "EURUSD=X", "EUR/TRY": "EURTRY=X"
+    "Gold": "GC=F", "Silber": "SI=F", "Öl": "BZ=F", "VIX": "^VIX", "EUR/USD": "EURUSD=X", "DXY": "DX-Y.NYB", "EUR/TRY": "EURTRY=X"
 }
 
 m_cols = st.columns(7)
